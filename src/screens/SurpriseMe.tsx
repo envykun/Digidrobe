773c33f17e6cac65d4d54ee115b8aaa4540f35cb
Zdenek Scherrer <zscherrer.uni@gmail.com>
2024-01-27 11:27:59 +0100

    no message

--- src/screens/SurpriseMe.tsx
+++ src/screens/SurpriseMe.tsx
@@ -12,16 +12,30 @@ import DigiButton from "@Components/Button/DigiButton";
 export default function Recent() {
   const refresh = useReducer((x) => x + 1, 0)[1];
   const db = getDatabase();
-  const { data: wardrobe, isLoading: loadingWardrobe, error: wardrobeError, refetch: refetchWardrobe } = useGet(getWardrobeItems(db));
-  const newOutfit = useRef<Outfit>(new Outfit({ uuid: randomUUID(), refresh: refresh })).current;
-  useEffect(() => console.log("DATA", wardrobe), [wardrobe]);
+  const {
+    data: wardrobe,
+    isLoading: loadingWardrobe,
+    error: wardrobeError,
+    refetch: refetchWardrobe,
+  } = useGet(getWardrobeItems(db));
+  const newOutfit = useRef<Outfit>(
+    new Outfit({ uuid: randomUUID(), refresh: refresh })
+  ).current;
+  
+  useEffect(() => {
+    if (!wardrobe) return;
+    newOutfit.generateRandomOutfit(wardrobe);
+  }, [wardrobe]);
 
   const renderItems = () => (
     <View>
       {newOutfit.getAllItems().map((item) => (
-        <View style={{ width: 90, height: 90 }}>
-          <Image source={{ uri: item.getImage() }} style={{ resizeMode: "cover", width: "100%", height: "100%" }} />
-          <Text>{item.getImage()}</Text>
+        <View key={item.uuid} style={{ width: 90, height: 90 }}>
+          <Image
+            source={{ uri: item.getImage() }}
+            style={{ resizeMode: "cover", width: "100%", height: "100%" }}
+          />
+          <Text>{item.name}</Text>
         </View>
       ))}
     </View>
@@ -31,9 +45,17 @@ export default function Recent() {
 
   return (
     <SafeAreaView>
-      <ScrollView style={layout.scrollContainer} showsVerticalScrollIndicator={false} contentContainerStyle={{ padding: 8, gap: 8 }}>
+      <ScrollView
+        style={layout.scrollContainer}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 8, gap: 8 }}
+      >
         {renderItems()}
-        <DigiButton title="Surprise me!" variant="text" onPress={() => newOutfit.generateRandomOutfit(wardrobe)} />
+        <DigiButton
+          title="Surprise me!"
+          variant="text"
+          onPress={() => newOutfit.generateRandomOutfit(wardrobe)}
+        />
       </ScrollView>
     </SafeAreaView>
   );
